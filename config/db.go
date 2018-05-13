e806204bb660d614cfb1d485a83f0a82bca6b4ad
Kevin Rizkhy <kevinrizkhy@users.noreply.github.com>
2018-05-14 02:29:15 +0700

    App 2

---
+++ config/db.go
@@ -0,0 +1,9 @@
+package config
+
+const (
+	DB_ADAPTER  = "postgres"
+	DB_USER     = "postgres"
+	DB_PASSWORD = "root"
+	DB_NAME     = "wellcode"
+	DB_SSLMODE  = "disable"
+)
---
+++ config/route.go
@@ -0,0 +1,8 @@
+package config
+
+const (
+	Base_Folder = "github.com/kevinrizkhy/Internal/"
+	Base_URL    = "http://127.0.0.1:8080/"
+
+	WK_PATH = "F:/GoFILE/src/github.com/SebastiaanKlippert/wkhtmltopdf/bin/wkhtmltopdf.exe"
+)
